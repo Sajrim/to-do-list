d4694b2295f47c0dc1a3457273777d2d59e5c558
Szymon Rutkowski <Szymon.rutkowski1@gmail.com>
2025-11-22 21:00:40 +0100

    Add task rendering logic

--- js/script.js
+++ js/script.js
@@ -61,3 +61,19 @@ const bindToggleDoneEvents = () => {
     });
   });
 };
+
+const renderTasks = () => {
+  const taskToHTML = (task) => `
+    <li class="tasks__item ${task.done && hideDoneTasks ? "tasks__item--hidden" : ""} js-task">
+      <button class="tasks__button tasks__button--toggleDone js-toggleDone">
+        ${task.done ? "✔️" : ""}
+      </button>
+      <span class="tasks__content${task.done ? " tasks__content--done" : ""}">
+        ${task.content}
+      </span>
+      <button class="tasks__button tasks__button--remove js-remove">🗑️</button>
+    </li> `;
+
+  const tasksElement = document.querySelector(".js-tasks");
+  tasksElement.innerHTML = tasks.map(taskToHTML).join("");
+};
